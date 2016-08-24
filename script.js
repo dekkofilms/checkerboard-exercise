6707dedb986ebe6e87d5888f02c1bbc9b5471d2c
Taylor King <dekkofilms@gmail.com>
2016-08-24 17:37:28 -0500

    red and black checkerboard done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Your JS goes here
+var body = document.getElementsByTagName('body')[0]
+
+for (var i = 0; i < 100; i++) {
+  var div = document.createElement('div');
+  div.style.width = "11.1%";
+  div.style.paddingBottom = "11.1%";
+  div.style.float = "left";
+
+  if (i % 2 === 0) {
+    div.style.backgroundColor = "red";
+  } else {
+    div.style.backgroundColor = "black";
+  }
+
+  body.appendChild(div);
+}
