a034b6bae265b4df1ceea14476f485061c444838
Taylor King <dekkofilms@gmail.com>
2016-08-24 21:16:02 -0500

    gradient works

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Your JS goes here
 var body = document.getElementsByTagName('body')[0]
 
-for (var i = 0; i < 100; i++) {
+for (var i = 0; i < 200; i++) {
   var div = document.createElement('div');
   div.style.width = "11.1%";
   div.style.paddingBottom = "11.1%";
   div.style.float = "left";
 
   if (i % 2 === 0) {
-    div.style.backgroundColor = "red";
+    div.style.backgroundColor = "rgba(173, 33, 99, " + (.02 * i) + ")";
   } else {
-    div.style.backgroundColor = "#342534";
+    div.style.backgroundColor = "rgba(13, 133, 199, " + (.02 * i) + ")";
   }
 
   body.appendChild(div);
